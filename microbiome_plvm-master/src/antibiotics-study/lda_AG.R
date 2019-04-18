2ef75ab87fe44c8e400169e79cd78693aadbeea3
Christine <ctataru5@stanford.edu>
2019-04-18 09:35:17 -0700

    plots for cgrb conference poster

--- microbiome_plvm-master/src/antibiotics-study/lda_AG.R
+++ microbiome_plvm-master/src/antibiotics-study/lda_AG.R
@@ -18,33 +18,14 @@ set.seed(11242016)
 setwd("C:/Users/ctata/Documents/Lab/quality_vectors/microbiome_plvm-master/src/AG")
 abt <- get(load("../../data/antibiotics-study/abt.rda"))
 ag <- readRDS("ag_data.rds")
-abt <- abt %>%
-  subset_samples(ind == argv$subject) #The 54 samples here refer to 54 timepoints of the same individual
 
-releveled_sample_data <- abt %>%
-  sample_data %>%
-  mutate(
-    condition = recode(
-      condition,
-      "Pre Cp" = "Pre",
-      "1st Cp" = "1st Course",
-      "1st WPC" = "1st Course",
-      "2nd Cp" = "2nd Course",
-      "2nd WPC" = "2nd Course",
-      "Post Cp" = "Post"
-    )
-  )
-rownames(releveled_sample_data) <- abt %>%
-  sample_data %>%
-  rownames
-sample_data(abt) <- releveled_sample_data
 
 
 ## ---- lda ----
 ag_small <- subset_samples(ag, BMI > 10 & BMI < 70)
 
 #Severly restrict samples by sequencing depth
-ag_small <- prune_samples(colSums(otu_table(ag_small)) > 13000, ag_small)
+ag_small <- prune_samples(colSums(otu_table(ag_small)) > 14500, ag_small)
 ag_small <- prune_samples(colSums(otu_table(ag_small)) < 15000, ag_small)
 #Remove any taxa not appearing in those samples
 ag_small <- filter_taxa(ag_small, function(abund) return(sum(abund) > 10), prune = T)
@@ -54,7 +35,7 @@ dimnames(x) <- NULL
 
 
 
-fitModel <- function(K){
+fitModel <- function(K, output_samples = 10){
   print(paste("Number of topics: ", K))
   stan_data <- list(
     K = K,
@@ -67,37 +48,37 @@ fitModel <- function(K){
   
   start_fit <- Sys.time()
   f <- stan_model(file = "../stan/lda_counts.stan")
+
+  stan_fit <- vb(
+    f,
+    data = stan_data,
+    output_samples = output_samples,
+    eta = 1,
+    adapt_engaged = FALSE)
+
+s
+  cat(sprintf(
+    "Finished in %f minutes\n",
+    Sys.time() - start_fit, 4)
+  )
   
+  return(stan_fit)
+}
+
+Ks <- c(3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+for(K in Ks){
   possibleError <- tryCatch(
-    
-    stan_fit <- vb(
-      f,
-      data = stan_data,
-      output_samples = 50,
-      eta = 1,
-      adapt_engaged = FALSE
-    ),
+    stan_fit <- fitModel(K, output_samples = 10),
     error=function(e) e
-    
   )
   
   if(inherits(possibleError, "error")){
     print(paste(K, " topics did not converge"))
     next
-  }
-  else{
-    cat(sprintf(
-      "Finished in %f minutes\n",
-      Sys.time() - start_fit, 4)
-    )
-  }
-  return(stan_fit)
+  } 
 }
-Ks <- c(2, 3, 4, 5, 6, 7, 8, 9)
-for(K in Ks){
-  fitModel(K)
-}
-stan_fit <- fitModel(K = 7)
+
+stan_fit <- fitModel(K = 7, output_samples = 500)
 
 
 
@@ -108,6 +89,7 @@ save(
   file = sprintf("../../data/fits/lda-%s-%s.rda", "AG", gsub("[:|| ||-]", "", Sys.time()))
 )
 samples <- rstan::extract(stan_fit)
+saveRDS(samples, "fit_samples.rds")
 rm(stan_fit)
 
 
@@ -178,9 +160,13 @@ beta_hat$rsv <- factor(beta_hat$rsv, levels = taxa$rsv)
 
 #Read in AG data and make phyloseq object
 library(phyloseq)
-otu_filtered_AG_07perc_feces <- read.delim("C:/Users/ctata/Documents/Lab/quality_vectors/data/AG_new/feces/otu_filtered_AG_07perc_feces.csv")
-otu <- otu_filtered_AG_07perc_feces
-colnames(otu) <- gsub("X", "", colnames(otu))
+library(biomformat)
+library(readr)
+library(dada2)
+otu_filtered_test_AG_07perc_feces <- read_table2("C:/Users/ctata/Documents/Lab/quality_vectors/data/AG_new/feces/otu_filtered_AG_07perc_feces.csv")
+otu <- otu_filtered_test_AG_07perc_feces
+rownames(otu) <- otu$OTUID
+otu <- otu %>% select(-OTUID)
 
 AG_mapping <- read.delim("C:/Users/ctata/Documents/Lab/quality_vectors/data/AG_new/AG_mapping.txt")
 map <- AG_mapping
@@ -191,10 +177,18 @@ map <- map[!is.na(map$BMI), ]
 map <- map[map$BMI < 100, ]
 map <- map[map$BMI > 1, ]
 rownames(map) <- map$X.SampleID
-otu <- otu[ , colnames(otu) %in% rownames(map)]
-biom <- import_biom("C:/Users/ctata/Documents/Lab/quality_vectors/data/AG_new/07-taxa/07-taxa/notrim/otu_table_fecal_L3.biom")
+otu_use <- otu %>% select(colnames(otu)[colnames(otu) %in% rownames(map)])
+
+ps <- phyloseq(otu_table(otu_use, taxa_are_rows = T), sample_data(map))
+
+repseqs <- read.fasta("C:/Users/ctata/Documents/Lab/quality_vectors/data/AG_new/feces/repseqs.fasta", as.string = T)
+names(repseqs) <- gsub("seq", "", names(repseqs))
+repseqs_use <- toupper(unlist(repseqs[rownames(otu)]))
+tt <- assignTaxonomy(repseqs_use, "C:/Users/ctata/Documents/Lab/public_data/silva_nr_v132_train_set.fa.gz")
+rownames(tt) <- names(repseqs_use)
+tax_table(ps) <- tax_table(tt)
 
 
-ps <- phyloseq(otu_table(otu, taxa_are_rows = T), sample_data(map))
+
 setwd("C:/Users/ctata/Documents/Lab/quality_vectors/microbiome_plvm-master/src/AG")
 saveRDS(ps, "ag_data.rds")
